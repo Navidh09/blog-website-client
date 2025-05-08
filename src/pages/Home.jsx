d480a9ff9eff477e14512ee850f0db056b3611aa
Navid Hasan <navid.nyon@gmail.com>
2025-05-08 19:26:03 +0600

    firebase setup done

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h4 className="font-extrabold text-7xl">This is home</h4>
+    </div>
+  );
+};
+
+export default Home;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,8 +1,11 @@
 import { createBrowserRouter } from "react-router";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <MainLayout></MainLayout>,
     errorElement: <h2>Page Not Found</h2>,
     children: [
       {
